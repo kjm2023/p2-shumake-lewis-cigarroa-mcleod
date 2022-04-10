bd842a8eea87ee3862c0b24d85a1c0c3aabd1ca9
Kaitlin Shumake <shumakek@ufl.edu>
2022-04-10 11:28:03 -0400

    fixing errors for nav and habitats.html

--- habitats.js
+++ habitats.js
@@ -1,14 +1,14 @@
 function readMore() {
-    var dots = document.getElementById('ellipsis');
+  var dots = document.getElementById("ellipsis");
   var buttonText = document.getElementById("readmorebtn");
   var moreText = document.getElementById("morecontent");
 
   if (dots.style.display === "none") {
-      dots.style.display = "inline";
-      buttonText.innerHTML = "Read More"
-      moreText.style.display = "none";
+    dots.style.display = "inline";
+    buttonText.innerHTML = "Read More";
+    moreText.style.display = "none";
   } else {
-      dots.style.display = "none";
+    dots.style.display = "none";
     buttonText.innerHTML = "Read Less";
     moreText.style.display = "inline";
   }
